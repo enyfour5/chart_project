5c4497e753517fa2ea6249f6ce8d6d6435cffc87
Rene Rosales Jr <enyfour5@gmail.com>
2019-11-06 03:27:24 +0000

    creating an inital repo for a web app I will be helping create

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var http = require('http');
+var path = require('path');
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+//routes
+
+app.get('/', (req, res) => {
+	res.render('index')
+});
+
+
+var server = http.createServer(app);
+
+var port = 8995;
+server.listen(port);
+
+
+module.exports = app;
